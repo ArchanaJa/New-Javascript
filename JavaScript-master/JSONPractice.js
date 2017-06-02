73bcedd2b6aebd07a9ea4d23bcd071733a59987c
ArchanaJa <archu5432@gmail.com>
2017-06-02 16:24:21 +0100

    Add files via upload

---
+++ JavaScript-master/JSONPractice.js
@@ -0,0 +1,33 @@
+// This is my practice file for JSON
+
+ var car = {
+    
+    "model": "Audi",
+    "price": 25000,
+    "engine": "Petrol"
+    }
+
+/*console.log(car.model);*/
+        
+    var F1drivers = ["Lewis", "Kimi", "Alonso"];
+        console.log(F1drivers[1]);
+        var F1 ={
+            "data":[
+                {
+                    "car":"Mercedes",
+                    "driver": "Lewis",
+                    "wins": 2
+                },
+                {
+                   "circuit":"Monaco", 
+                    "pointsystem":[
+                        {
+                            "first": 25,
+                            "secondplace" : 18,
+                            "thirdplace":15
+                        }
+                    ]
+                }
+            ]
+            
+        }
